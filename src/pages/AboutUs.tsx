fc273e4f764590aaa910edd8f623c376a76a2fbf
aamiraj <49194215+aamiraj@users.noreply.github.com>
2024-09-02 22:03:56 +0600

    completed about us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,12 +1,76 @@
+import { MdEmail } from "react-icons/md";
+import Model1 from "../assets/models/model-1.jpg";
+import Model2 from "../assets/models/model-2.jpg";
+import { FaPhone, FaShop } from "react-icons/fa6";
+import { BsSend } from "react-icons/bs";
+import Slider from "react-slick";
+import Profile from "../assets/profile-pic/profile-1.jpg";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const testimonials = [
+  {
+    id: 1,
+    img: Profile,
+    name: "Alif Hasan",
+    message:
+      "I have bought a motorized treadmill from Fitness Shop. I got a discount of 25%. Best deal I have made.",
+  },
+  {
+    id: 2,
+    img: Profile,
+    name: "Zabir Mahmud",
+    message:
+      "Their imported products are original. Anyone can buy equipments reliably. I myself bought a pair of dumbbell. Looking for a spinning bike.",
+  },
+  {
+    id: 3,
+    img: Profile,
+    name: "Ali Hosain",
+    message:
+      "I just got a bench. I use this for my workout. It is heavy duty. I will buy a pair of dumbbell from them.",
+  },
+];
+
 const AboutUs = () => {
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    cssEase: "ease-in-out",
+  };
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+      offset: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className="p-4 md:p-8">
+    <div className="">
       <div className="container">
-        <h1 className="py-4 text-center text-primary font-semibold tracking-widest uppercase text-2xl sm:text-4xl">
-          Fitness Shop
-        </h1>
         {/* overview section  */}
-        <div className="px-2 py-8 md:px-8 shadow-lg">
+        <div
+          className="flex flex-col items-center justify-center px-2 py-8 md:px-8"
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          data-aos-easing="linear"
+          data-aos-delay="500"
+        >
+          <h1 className="py-4 text-center text-primary font-semibold tracking-widest uppercase text-2xl sm:text-4xl">
+            Fitness Shop
+          </h1>
           <p className="text-center font-bold text-xl sm:text-2xl py-4">
             Overview
           </p>
@@ -36,52 +100,185 @@ const AboutUs = () => {
           </p>
         </div>
         {/* our team section  */}
-        <div>
-          <p className="text-center font-bold text-xl sm:text-2xl py-4">
+        <div className="px-2 py-8 md:px-8">
+          <h2 className="text-center font-bold text-xl sm:text-2xl py-4">
             Our Team
-          </p>
+          </h2>
           {/* first founding member  */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
+          <div className="py-4 grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 items-start xl:items-center">
             {/* picture of founder 1 */}
-            <div>
-              <img src="" alt="" />
+            <div
+              className="p-4 flex items-center justify-center md:justify-end"
+              data-aos="fade-left"
+              data-aos-duration="500"
+            >
+              <img
+                src={Model1}
+                alt="model1"
+                className="max-w-[300px] object-contain"
+              />
             </div>
             {/* roles and bios section   */}
-            <div>
-              <p>Founding member 1</p>
+            <div
+              className="py-4 flex flex-col gap-2 items-start text-justify"
+              data-aos="fade-right"
+              data-aos-duration="500"
+            >
+              <p className="font-bold text-2xl lg:text-4xl text-brandBlue">
+                Ted Rich
+              </p>
+              <p className="font-semibold italic text-xl lg:text-2xl">
+                Founder, Fitness Shop
+              </p>
+              <p className="font-semibold italic text-xl lg:text-2xl">
+                Chief Officer, Marketing, HR, and Logistics
+              </p>
+              <p className="text-base font-normal">
+                Ted Rich is one of the founding member of the company. His new
+                ideas and skills inspires the whole team. He is enthusiasts and
+                hard-working person. Academically he is one of the topper in his
+                University. He likes to debate and always likes to ask
+                questions. Currently he is in the position of handling
+                Marketing, Human Resource and Logistics departments. He is
+                responsible for 20% growth of the company last year. He tries to
+                find out the right person for the company. He recruites 4
+                valuable members who helped him achieve the growth. He has a
+                marvelous skill to handle the logistics efficiently. All in all
+                he is one of the key members in our team.
+              </p>
             </div>
           </div>
           {/* second founding member  */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 ">
+          <div
+            className="py-4 grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 items-start xl:items-center"
+            data-aos="fade-right"
+            data-aos-duration="800"
+          >
             {/* roles and bios section   */}
-            <div>
-              <p>Founding member 2</p>
+            <div className="py-4 order-2 md:order-1 flex flex-col gap-2 items-end text-justify">
+              <p className="font-bold text-2xl lg:text-4xl text-brandBlue">
+                John Doe
+              </p>
+              <p className="font-semibold italic text-xl lg:text-2xl">
+                Founder, Fitness Shop
+              </p>
+              <p className="font-semibold italic text-xl lg:text-2xl">
+                Chief Officer, IT, Accounting and Finance
+              </p>
+              <p className="text-base font-normal">
+                John Doe is also one of the founding memeber. He is mainly a
+                tech guy in our company. Also he knows how to handle accounts
+                and finance. As our main shop is digital, a lot of the
+                transactions happen digitally. He can handle them all with his
+                custom built software. In his academic life he was very bright.
+                He became fond of coding and problem solving at an early age,
+                and since then he is working remarkably. He is the reason we
+                have a digital shop, otherwise we have to go for a traditional
+                shop. He always motivates our team by his hard-working
+                personality. He tells the team that this company is a big
+                problem and we need to work hard to solve this problem. As a
+                consequence, everyone loves him and seek guidance from him.
+              </p>
             </div>
             {/* picture of founder 2 */}
-            <div>
-              <img src="" alt="" />
+            <div
+              className="order-1 md:order-2 p-4 flex items-center justify-center md:justify-start"
+              data-aos="fade-left"
+              data-aos-duration="800"
+            >
+              <img
+                src={Model2}
+                alt="model2"
+                className="max-w-[300px] object-contain"
+              />
             </div>
           </div>
         </div>
         {/* testimonials section  */}
-        <div>
+        <div
+          className="px-2 py-8 md:px-8"
+          data-aos="zoom-in"
+          data-aos-duration="500"
+        >
           <p className="text-center font-bold text-xl sm:text-2xl py-4">
             Testimonials
           </p>
           {/* carousel of testimonials */}
-          <div>Testimonials Carousels</div>
+          <div>
+            <Slider {...settings}>
+              {testimonials.map((data) => (
+                <Testimonials
+                  key={data.id}
+                  img={data.img}
+                  name={data.name}
+                  message={data.message}
+                />
+              ))}
+            </Slider>
+          </div>
         </div>
         {/* contact info section  */}
-        <div>
+        <div
+          className="px-2 py-8 md:px-8"
+          data-aos="zoom-in"
+          data-aos-duration="500"
+        >
           <p className="text-center font-bold text-xl sm:text-2xl py-4">
             Contact
           </p>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
-            <div>
-                <p>Form for sending questions or feedbacks</p>
-            </div>
-            <div>
-                <address>Physical address </address>
+            <form className="max-w-full p-4 flex flex-col gap-2">
+              <div className="w-full flex flex-col gap-2">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="px-4 py-2 dark:bg-gray-800 dark:text-white dark:border-gray-800 outline-1 outline-sky-500 border rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="px-4 py-2 dark:bg-gray-800 dark:text-white dark:border-gray-800 outline-1 outline-sky-500 border rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="feedback">Feedback</label>
+                <textarea
+                  name="feedback"
+                  id="feedback"
+                  rows={3}
+                  className="px-4 py-2 dark:bg-gray-800 dark:text-white dark:border-gray-800 outline-1 outline-sky-500 border rounded-lg"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="gradient-btn w-full my-4 text-white cursor-pointer duration-300 py-2 px-8 rounded-full relative z-10 uppercase flex items-center gap-2 justify-center"
+              >
+                Submit
+                <BsSend />
+              </button>
+            </form>
+            <div className="py-4 lg:p-4 flex flex-col gap-3">
+              <p className="flex items-center gap-2">
+                <MdEmail />
+                <span>Email:</span>
+                <span>fitness.shop@email.com</span>
+              </p>
+              <p className="flex items-center gap-2">
+                <FaPhone />
+                <span>Phone:</span>
+                <span>+88017XX-XXXXXX</span>
+              </p>
+              <p className="flex items-center gap-2">
+                <FaShop />
+                <span>Address:</span>
+                <span>Gulshan, Dhaka, Bangladesh</span>
+              </p>
             </div>
           </div>
         </div>
@@ -91,3 +288,31 @@ const AboutUs = () => {
 };
 
 export default AboutUs;
+
+const Testimonials = ({
+  img,
+  name,
+  message,
+}: {
+  img: string;
+  name: string;
+  message: string;
+}) => {
+  return (
+    <div>
+      <div className="w-full h-[300px] flex flex-col gap-2 items-center justify-center">
+        <div className="">
+          <img
+            src={img}
+            alt="profile"
+            className="w-24 h-24 rounded-full object-cover"
+          />
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold text-2xl text-center">{name}</p>
+          <p className="font-normal text-lg italic text-center">"{message}"</p>
+        </div>
+      </div>
+    </div>
+  );
+};

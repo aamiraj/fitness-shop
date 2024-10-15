e76c176c4a5962dac9dbff16f1ccd6b6f93a5bea
aamiraj <49194215+aamiraj@users.noreply.github.com>
2024-10-15 20:47:42 +0600

    toaster moved upward

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <main className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
+      <Toaster position="top-center" reverseOrder={false} />
       <Navbar />
       <Outlet />
       <Footer />
